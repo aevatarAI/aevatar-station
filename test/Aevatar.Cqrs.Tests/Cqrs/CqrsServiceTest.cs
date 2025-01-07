7e112df075f83be3addb1cdf967508c4b8a2e84d
suerlych <19871208@lhcyf>
2025-01-07 23:21:39 +0800

    Save state to es

--- test/Aevatar.Cqrs.Tests/Cqrs/CqrsServiceTest.cs
+++ test/Aevatar.Cqrs.Tests/Cqrs/CqrsServiceTest.cs
@@ -31,7 +31,7 @@ public class CqrsServiceTest : AevatarApplicationTestBase
 
         _clusterClient = GetRequiredService<IClusterClient>();
         _mockIndexingService = new Mock<IIndexingService>();
-        _mockIndexingService.Setup(service => service.SaveOrUpdateStateIndexAsync(It.IsAny<string>(), It.IsAny<BaseStateIndex>()))
+        _mockIndexingService.Setup(service => service.SaveOrUpdateStateIndexAsync(It.IsAny<string>(), It.IsAny<StateBase>()))
             .Returns(Task.CompletedTask);
         _mockIndexingService.Setup(b => b.QueryStateIndexAsync(It.IsAny<string>(), It.IsAny<string>()))
             .ReturnsAsync((string id, string indexName) => new BaseStateIndex { Id = IndexId.ToString(), Ctime = DateTime.Now, State = Address});
