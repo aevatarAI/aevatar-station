c53ee1d089f213b27cd78da9f56cc28ed9fb98e7
louis.li <louis.li@aelf.io>
2025-01-08 11:07:23 +0800

    feat: del useless code

--- src/Aevatar.Domain.Grains/Event/BasicReceivedEvent.cs
+++
@@ -1,7 +0,0 @@
-namespace Aevatar.Domain.Grains.Event;
-
-[GenerateSerializer]
-public class BasicReceivedEvent: BasicEvent
-{
-
-}
--- src/Aevatar.Domain.Grains/Event/MarketOperatoerCompleteEvent.cs
+++
@@ -1,6 +0,0 @@
-namespace Aevatar.Domain.Grains.Event;
-
-[GenerateSerializer]
-public class MarketOperatoerCompleteEvent : BasicCompletedEvent
-{
-}
