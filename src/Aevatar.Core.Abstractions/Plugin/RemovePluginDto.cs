d765909823a041b8cb499bf129de650751505317
eanzhao <yiqi.zhao@aelf.io>
2025-01-18 11:01:04 +0800

    fix RemovePluginDto

--- src/Aevatar.Core.Abstractions/Plugin/RemovePluginDto.cs
+++ src/Aevatar.Core.Abstractions/Plugin/RemovePluginDto.cs
@@ -4,5 +4,5 @@ namespace Aevatar.Core.Abstractions.Plugin;
 public class RemovePluginDto
 {
     [Id(0)] public required Guid TenantId { get; set; }
-    [Id(0)] public required Guid PluginCodeId { get; set; }
+    [Id(1)] public required Guid PluginCodeId { get; set; }
 }
