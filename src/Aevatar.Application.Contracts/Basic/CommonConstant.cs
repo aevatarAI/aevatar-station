eb4a5cc509a18fed8ac4ab524215129d138140b7
louis.li <louis.li@aelf.io>
2025-01-23 15:14:12 +0800

    feat:del useless code

--- src/Aevatar.Application.Contracts/Basic/CommonConstant.cs
+++
@@ -1,7 +0,0 @@
-namespace Aevatar.Basic;
-
-public static class CommonConstant
-{
-    public const int HttpSuccessCode = 200;
-    public const int HttpFileUploadSuccessCode = 20000;
-}
--- src/Aevatar.EventSourcing.MongoDB/MongoDbStorageConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Aevatar.EventSourcing.MongoDB;
-
-public static class MongoDbStorageConstants
-{
-    public const string StateCollectionName = "AevatarState";
-    public const string EventCollectionName = "AevatarEvent";
-}
