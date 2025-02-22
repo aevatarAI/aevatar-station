1319fdf9ff93e3bffce239611270381a83b9c5f2
louis.li <louis.li@aelf.io>
2025-02-22 22:47:48 +0800

    feat:update host

--- src/Aevatar.Silo/Extensions/OrleansHostExtension.cs
+++ src/Aevatar.Silo/Extensions/OrleansHostExtension.cs
@@ -146,9 +146,9 @@ public static class OrleansHostExtension
                 {
                     siloBuilder.AddMemoryStreams("Aevatar");
                 }
-                siloBuilder.UseAevatar();
-              //  siloBuilder.UseSignalR(); 
-              //  siloBuilder.RegisterHub<AevatarSignalRHub>();
+               // siloBuilder.UseAevatar();
+               // siloBuilder.UseSignalR(); 
+               // siloBuilder.RegisterHub<AevatarSignalRHub>();
             }).ConfigureServices((context, services) =>
             {
                 services.Configure<AzureOpenAIConfig>(context.Configuration.GetSection("AIServices:AzureOpenAI"));
