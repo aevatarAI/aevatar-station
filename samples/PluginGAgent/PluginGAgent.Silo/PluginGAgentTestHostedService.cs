f2b957142182a682b3b784875f6479543c91562c
eanzhao <yiqi.zhao@aelf.io>
2025-02-27 18:48:00 +0800

    feat: improve impl of PluginGAgentManager.GetPluginAssembliesAsync

--- samples/PluginGAgent/PluginGAgent.Silo/PluginGAgentTestHostedService.cs
+++ samples/PluginGAgent/PluginGAgent.Silo/PluginGAgentTestHostedService.cs
@@ -1,4 +1,5 @@
 using Aevatar.Core.Abstractions.Extensions;
+using Aevatar.Core.Abstractions.Plugin;
 using Aevatar.Plugins.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,14 @@ public class PluginGAgentTestHostedService : IHostedService
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         await _application.InitializeAsync(_serviceProvider);
+        var tenantId = "test".ToGuid();
+        var pluginManager = _serviceProvider.GetRequiredService<IPluginGAgentManager>();
+        var plugins = await pluginManager.GetPluginAssembliesAsync(tenantId);
+        Console.WriteLine("Plugins:");
+        foreach (var plugin in plugins)
+        {
+            Console.WriteLine(plugin.FullName);
+        }
         var permissionManager = _serviceProvider.GetRequiredService<IPermissionManager>();
         var userId = "TestUser".ToGuid().ToString();
         await permissionManager.SetAsync("DoSomething", "User", userId, true);
