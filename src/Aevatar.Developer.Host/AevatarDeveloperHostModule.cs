23a9b580c86dcb702f388a52f5cfc3770d993e1c
louis.li <louis.li@aelf.io>
2025-02-28 15:37:35 +0800

    feat:del error page

--- src/Aevatar.Developer.Host/AevatarDeveloperHostModule.cs
+++ src/Aevatar.Developer.Host/AevatarDeveloperHostModule.cs
@@ -153,11 +153,6 @@ public class AevatarDeveloperHostModule : AbpModule
 
         app.UseAbpRequestLocalization();
 
-        if (!env.IsDevelopment())
-        {
-            app.UseErrorPage();
-        }
-
         app.UseCorrelationId();
         app.UseStaticFiles();
         app.UseRouting();
