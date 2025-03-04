df6095e87a787a4725624e25ce479bf6a9dfb6de
louis.li <louis.li@aelf.io>
2025-03-04 19:33:57 +0800

    feat:update test agent

---
+++ src/Aevatar.Application.Grains/Agents/TestAgent/AgentTest.cs
@@ -0,0 +1,92 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Aevatar.Core;
+using Aevatar.Core.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Aevatar.Application.Grains.Agents.TestAgent;
+
+public class AgentTest : GAgentBase<FrontAgentState, FrontTestEvent, EventBase>, IFrontAgentTest
+{
+    public override Task<string> GetDescriptionAsync()
+    {
+        return Task.FromResult("this is used for front test");
+    }
+    
+    [EventHandler]
+    public async Task HandleFrontTestCreateEvent(FrontTestCreateEvent @event)
+    {
+        RaiseEvent(new FrontTestCreateSEvent
+        {
+            Id = Guid.NewGuid(),
+            Name = @event.Name
+        });
+        await ConfirmEvents();
+    }
+
+    protected override void GAgentTransitionState(FrontAgentState state, StateLogEventBase<FrontTestEvent> @event)
+    {
+        switch (@event)
+        {
+            case FrontTestCreateSEvent frontTestCreateSEvent:
+                state.Name = frontTestCreateSEvent.Name;
+                state.Id = Guid.NewGuid();
+                break;
+        }
+    }
+}
+
+public interface IFrontAgentTest : IGAgent
+{
+}
+
+[GenerateSerializer]
+public class FrontAgentState : StateBase
+{
+    [Id(0)] public Guid Id { get; set; }
+    [Id(1)] [Required] public string Name { get; set; }
+   
+}
+
+[GenerateSerializer]
+public class FrontTestEvent : StateLogEventBase<FrontTestEvent>
+{
+}
+
+[GenerateSerializer]
+public class FrontTestCreateSEvent : FrontTestEvent
+{
+    [Id(0)] public string Name { get; set; }
+}
+[GenerateSerializer]
+public class FrontTestCreateEvent : EventBase
+{
+    [Id(0)] [Required] public string Name { get; set; }
+}
+
+
+
+[GenerateSerializer]
+public class FrontInitConfig : ConfigurationBase
+{
+    [Id(0)] [Required] public string Name { get; set; }
+
+    [Id(1)] [Required] public List<int> StudentIds { get; set; }
+
+    [Id(2)] [Required] public JobType JobType { get; set; }
+
+    [Required]
+    [RegularExpression(@"^https?://.*")]
+    [Description("the url of school")]
+    [Id(3)]
+    public string Url { get; set; }
+
+    [Id(4)] public string Memo { get; set; }
+}
+
+public enum JobType
+{
+    Teacher,
+    Professor,
+    Dean
+}
