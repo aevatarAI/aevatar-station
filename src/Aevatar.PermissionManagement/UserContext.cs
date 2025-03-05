04ab3065069b76006cfe4101a0de5717db3a43a2
eanzhao <yiqi.zhao@aelf.io>
2025-03-05 14:27:48 +0800

    tune UserContext

--- src/Aevatar.PermissionManagement/UserContext.cs
+++ src/Aevatar.PermissionManagement/UserContext.cs
@@ -3,8 +3,8 @@ namespace Aevatar.PermissionManagement;
 [GenerateSerializer]
 public class UserContext
 {
-    [Id(0)] public required Guid UserId { get; set; }
-    [Id(1)] public required string[] Roles { get; set; }
+    [Id(0)] public Guid UserId { get; set; }
+    [Id(1)] public string[] Roles { get; set; } = [];
     [Id(2)] public string UserName { get; set; } = string.Empty;
     [Id(3)] public string Email { get; set; } = string.Empty;
     [Id(4)] public string ClientId { get; set; } = string.Empty;
