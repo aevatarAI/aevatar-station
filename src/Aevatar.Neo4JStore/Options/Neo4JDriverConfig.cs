1fa0cdb58b44f49d97ea7e3476250791a2f0fb23
wilson <wilson.deng@aelf.io>
2025-03-08 17:04:27 +0800

    feat: remove redundancy

--- src/Aevatar.Neo4JStore/Options/Neo4JDriverConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace Aevatar.Neo4JStore.Options;
-
-public class Neo4JDriverConfig
-{
-    public const string ConfigSectionName = "Neo4j";
-    public string Uri { get; set; }
-    public string User { get; set; }
-    public string Password { get; set; }
-}
