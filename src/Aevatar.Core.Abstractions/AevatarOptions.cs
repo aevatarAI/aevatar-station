51a72cec5c956af325e6fb6021db92c417a57b80
zhifenglee-aelf <zhifeng.lee@aelf.io>
2025-03-11 09:04:16 +0800

    feat(projection grain): updated projection grain to project instead of gagentbase

--- src/Aevatar.Core.Abstractions/AevatarOptions.cs
+++ src/Aevatar.Core.Abstractions/AevatarOptions.cs
@@ -3,4 +3,5 @@ namespace Aevatar.Core.Abstractions;
 public class AevatarOptions
 {
     public string StreamNamespace { get; set; } = "AINamespace";
+    //public int ElasticSearchProcessors { get; set; } = 10;
 }
