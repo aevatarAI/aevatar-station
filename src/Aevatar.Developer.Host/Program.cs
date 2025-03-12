7810ce11fb2e449a658355a48959e19ffca59c23
louis.li <louis.li@aelf.io>
2025-03-12 17:52:32 +0800

    feat:add AevatarAuthorizationMiddlewareResultHandler

--- src/Aevatar.Developer.Host/Program.cs
+++ src/Aevatar.Developer.Host/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Aevatar.Developer.Host.Extensions;
+using Aevatar.Handler;
 using Aevatar.SignalR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -45,6 +47,7 @@ public class Program
                 .UseAutofac()
                 .UseSerilog();
             builder.Services.AddSignalR().AddOrleans();
+            builder.Services.AddSingleton<IAuthorizationMiddlewareResultHandler, AevatarAuthorizationMiddlewareResultHandler>();
             await builder.AddApplicationAsync<AevatarDeveloperHostModule>();
             var app = builder.Build();
             await app.InitializeApplicationAsync();
