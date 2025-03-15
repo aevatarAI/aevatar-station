7ee7f6c298bd8f9cd19bf97a203ebb2bb24a15b8
wilson <wilson.deng@aelf.io>
2025-03-15 16:03:56 +0800

    feat: add neo4j test service

--- src/Aevatar.Application/Service/Neo4JTestService.cs
+++ src/Aevatar.Application/Service/Neo4JTestService.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Neo4j.Driver;
 using Volo.Abp;
 using Volo.Abp.Application.Services;
@@ -15,14 +17,19 @@ public interface INeo4JTestService
 public class Neo4JTestService : ApplicationService, INeo4JTestService
 {
     private readonly IDriver _driver;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<Neo4JTestService> _logger;
 
-    public Neo4JTestService(IDriver driver)
+    public Neo4JTestService(IDriver driver, IConfiguration configuration, ILogger<Neo4JTestService> logger)
     {
         _driver = driver;
+        _configuration = configuration;
+        _logger = logger;
     }
 
     public async Task<string> TestConnectionAsync()
     {
+        _logger.LogInformation("Testing Neo4j Connection, uri {url}", _configuration["Neo4j:Uri"]);
         await using var session = _driver.AsyncSession();
         var result = await session.RunAsync("RETURN 'Neo4j Connection Successful' AS message");
         var record = await result.SingleAsync();
