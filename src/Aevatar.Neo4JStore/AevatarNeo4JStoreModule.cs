6655f22fdf0863a024e344391112a980e91c4313
wilson <wilson.deng@aelf.io>
2025-03-17 11:08:09 +0800

    feat: add config

--- src/Aevatar.Neo4JStore/AevatarNeo4JStoreModule.cs
+++ src/Aevatar.Neo4JStore/AevatarNeo4JStoreModule.cs
@@ -1,3 +1,4 @@
+using Aevatar.Neo4JStore.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Neo4j.Driver;
 using Volo.Abp.AutoMapper;
@@ -15,12 +16,17 @@ public class AevatarNeo4JStoreModule : AbpModule
         });
         
         var configuration = context.Services.GetConfiguration();
+        context.Services.Configure<Neo4JConfigOptions>(configuration.GetSection("Neo4j"));
         context.Services.AddSingleton<IDriver>(_ => GraphDatabase.Driver(
-            configuration["Neo4j:Uri"],
-            AuthTokens.Basic(
-                configuration["Neo4j:User"],
-                configuration["Neo4j:Password"]
+                configuration["Neo4j:Uri"],
+                AuthTokens.Basic(
+                    configuration["Neo4j:User"],
+                    configuration["Neo4j:Password"]),
+                o => o.WithMaxConnectionPoolSize(2)
+                    .WithConnectionTimeout(TimeSpan.FromMilliseconds(10))
+                    .WithConnectionAcquisitionTimeout(TimeSpan.FromMilliseconds(10))
+                    .WithMaxConnectionLifetime(TimeSpan.FromMilliseconds(10))
             )
-        ));
+        );
     }
 }
