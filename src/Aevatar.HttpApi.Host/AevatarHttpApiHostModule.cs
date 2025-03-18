f57f2b3f38565f42989633fb99473f7a157ba8ca
wilson <wilson.deng@aelf.io>
2025-03-18 14:03:19 +0800

    feat: change callback path

--- src/Aevatar.HttpApi.Host/AevatarHttpApiHostModule.cs
+++ src/Aevatar.HttpApi.Host/AevatarHttpApiHostModule.cs
@@ -118,6 +118,7 @@ public class AevatarHttpApiHostModule : AIApplicationGrainsModule, IDomainGrains
                 options.ClientId = configuration["Authentication:Google:ClientId"];
                 options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
                 options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.CallbackPath = "/quantumgpt-client/signin-google";
             })
             .AddCookie();
     }
