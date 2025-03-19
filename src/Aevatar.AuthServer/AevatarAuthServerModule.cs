c23623dac808b5a525ed5b4da84a540082ea83b7
wilson <wilson.deng@aelf.io>
2025-03-19 14:23:16 +0800

    feat: add google grant type

--- src/Aevatar.AuthServer/AevatarAuthServerModule.cs
+++ src/Aevatar.AuthServer/AevatarAuthServerModule.cs
@@ -5,7 +5,6 @@ using Aevatar.Options;
 using Localization.Resources.AbpUi;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Identity;
 using Volo.Abp;
 using Volo.Abp.Account;
 using Volo.Abp.Account.Web;
@@ -59,7 +58,6 @@ public class AevatarAuthServerModule : AbpModule
         {
             builder.AddServer(options =>
             {
-                options.SetAuthorizationCodeLifetime(TimeSpan.FromMinutes(10));
                 options.UseAspNetCore().DisableTransportSecurityRequirement();
                 options.SetIssuer(new Uri(configuration["AuthServer:IssuerUri"]!));
                 // options.IgnoreGrantTypePermissions();
@@ -83,6 +81,7 @@ public class AevatarAuthServerModule : AbpModule
             builder.Configure(openIddictServerOptions =>
             {
                 openIddictServerOptions.GrantTypes.Add(GrantTypeConstants.SIGNATURE);
+                openIddictServerOptions.GrantTypes.Add(GrantTypeConstants.GOOGLE);
             });
         });
     }
@@ -100,6 +99,9 @@ public class AevatarAuthServerModule : AbpModule
         context.Services.Configure<AbpOpenIddictExtensionGrantsOptions>(options =>
         {
             options.Grants.Add(GrantTypeConstants.SIGNATURE, new SignatureGrantHandler());
+            options.Grants.Add(GrantTypeConstants.GOOGLE, 
+                new GoogleGrantHandler(context.Services.GetRequiredService<IConfiguration>(), 
+                    context.Services.GetRequiredService<ILogger<GoogleGrantHandler>>()));
         });
 
         Configure<AbpLocalizationOptions>(options =>
