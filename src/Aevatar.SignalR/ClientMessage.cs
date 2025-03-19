cd919e518a1d6260d19d61851284f171948bdf53
eanzhao <yiqi.zhao@aelf.io>
2025-03-19 22:39:39 +0800

    feat: tune ClientMessage.

--- src/Aevatar.SignalR/ClientMessage.cs
+++ src/Aevatar.SignalR/ClientMessage.cs
@@ -1,4 +1,9 @@
 namespace Aevatar.SignalR;
 
 [Immutable, GenerateSerializer]
-public sealed record ClientMessage(string HubName, string ConnectionId, [Immutable] ClientNotification Message);
+public class ClientMessage(string hubName, string connectionId, ClientNotification message)
+{
+    [Id(0)] public string HubName { get; set; } = hubName;
+    [Id(1)] public string ConnectionId { get; set; } = connectionId;
+    [Immutable] [Id(2)] public ClientNotification Message { get; set; } = message;
+}
