23d2931bcc16f78071eaa77a7461e3ca41002341
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-03-20 00:35:04 +0800

    feat:add log

--- src/Aevatar.Developer.Host/Extensions/OrleansHostExtensions.cs
+++ src/Aevatar.Developer.Host/Extensions/OrleansHostExtensions.cs
@@ -53,14 +53,18 @@ public static class OrleansHostExtensions
                     .AddKafka("Aevatar")
                     .WithOptions(options =>
                     {
+                        Log.Debug("Step 1");
                         options.BrokerList = config.GetSection("OrleansStream:Brokers").Get<List<string>>();
                         options.ConsumerGroupId = "Aevatar";
                         options.ConsumeMode = ConsumeMode.LastCommittedMessage;
-
+                        Log.Debug("Step 2");
                         var partitions = config.GetSection("OrleansStream:Partitions").Get<int>();
+                        Log.Debug($"Step 2 partitions:{partitions}");
                         var replicationFactor =
                             config.GetSection("OrleansStream:ReplicationFactor").Get<short>();
+                        Log.Debug($"Step 3--->{replicationFactor}");
                         var topics = config.GetSection("OrleansStream:Topics").Get<string>();
+                        Log.Debug($"Step 4--->topic:{topics}");
                         topics = topics.IsNullOrEmpty() ? CommonConstants.StreamNamespace : topics;
                         foreach (var topic in topics.Split(','))
                         {
