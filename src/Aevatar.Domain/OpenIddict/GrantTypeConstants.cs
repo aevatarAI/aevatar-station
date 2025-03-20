4e52889d5a2ca674f84f903c22f34793dc467de3
house1313 <house@schrodingernft.ai>
2025-03-20 15:28:27 +0800

    add apple grant type

--- src/Aevatar.Domain/OpenIddict/GrantTypeConstants.cs
+++ src/Aevatar.Domain/OpenIddict/GrantTypeConstants.cs
@@ -6,4 +6,5 @@ public class GrantTypeConstants
     public const string LOGIN = "login_credentials";
     public const string PERMISSIONS_SIGNATURE = "gt:signature";
     public const string GOOGLE = "google";
+    public const string APPLE = "apple";
 }
