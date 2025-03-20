61fd5c13408b1ae67bc61a55ca2ecd80e7fe041c
zhxymh <sean.zhang@aelf.io>
2025-03-20 14:22:59 +0800

    test: register

--- test/Aevatar.Application.Tests/AevatarApplicationTestModule.cs
+++ test/Aevatar.Application.Tests/AevatarApplicationTestModule.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Nest;
 using Volo.Abp.Auditing;
 using Volo.Abp.AutoMapper;
+using Volo.Abp.Emailing;
 using Volo.Abp.EventBus;
 using Volo.Abp.Modularity;
 
@@ -38,5 +39,7 @@ public class AevatarApplicationTestModule : AbpModule
         context.Services.AddMediatR(typeof(GetStateQueryHandler).Assembly);
         context.Services.AddMediatR(typeof(GetGEventQueryHandler).Assembly);
         context.Services.AddTransient<IHostDeployManager, DefaultHostDeployManager>();
+
+        context.Services.AddSingleton<IEmailSender, NullEmailSender>();
     }
 }
