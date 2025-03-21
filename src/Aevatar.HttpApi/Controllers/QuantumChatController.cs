99f910545e515637db5ae0b8e71435f3d9c761e1
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-03-21 16:12:42 +0800

    feat:adjust code

--- src/Aevatar.HttpApi/Controllers/QuantumChatController.cs
+++ src/Aevatar.HttpApi/Controllers/QuantumChatController.cs
@@ -70,4 +70,10 @@ public class QuantumChatController : AevatarController
     {
         await _quantumService.RenameSessionAsync((Guid)CurrentUser.Id!, request.SessionId, request.Title);
     }
+
+    [HttpGet("userId")]
+    public Task<Guid> GetUserId()
+    {
+        return Task.FromResult((Guid)CurrentUser.Id!);
+    }
 }
