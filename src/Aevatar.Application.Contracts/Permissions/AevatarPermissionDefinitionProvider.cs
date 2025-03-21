b1b4840d69e2819cb39be9b504c4a0586995a7cb
louis.li <louis.li@aelf.io>
2025-03-21 20:19:33 +0800

    feat:add es log

--- src/Aevatar.Application.Contracts/Permissions/AevatarPermissionDefinitionProvider.cs
+++ src/Aevatar.Application.Contracts/Permissions/AevatarPermissionDefinitionProvider.cs
@@ -14,7 +14,8 @@ public class AevatarPermissionDefinitionProvider : PermissionDefinitionProvider
         adminManagement.AddPermission(AevatarPermissions.AdminPolicy, L("Permission:AdminPolicy"));
 
         // Agent Management
-        var agentManagementGroup = context.AddGroup(AevatarPermissions.Agent.GroupName, L("Permission:AgentManagement"));
+        var agentManagementGroup =
+            context.AddGroup(AevatarPermissions.Agent.GroupName, L("Permission:AgentManagement"));
         agentManagementGroup.AddPermission(AevatarPermissions.Agent.ViewLogs, L("Permission:ViewLogs"));
         agentManagementGroup.AddPermission(AevatarPermissions.Agent.ViewAllType, L("Permission:ViewAll"));
         agentManagementGroup.AddPermission(AevatarPermissions.Agent.Create, L("Permission:Create"));
@@ -23,38 +24,48 @@ public class AevatarPermissionDefinitionProvider : PermissionDefinitionProvider
         agentManagementGroup.AddPermission(AevatarPermissions.Agent.Delete, L("Permission:Delete"));
 
         // Relationship Management
-        var relationshipGroup = context.AddGroup(AevatarPermissions.Relationship.GroupName, L("Permission:AgentRelationshipManagement"));
-        relationshipGroup.AddPermission(AevatarPermissions.Relationship.ViewRelationship, L("Permission:ViewRelationship"));
+        var relationshipGroup = context.AddGroup(AevatarPermissions.Relationship.GroupName,
+            L("Permission:AgentRelationshipManagement"));
+        relationshipGroup.AddPermission(AevatarPermissions.Relationship.ViewRelationship,
+            L("Permission:ViewRelationship"));
         relationshipGroup.AddPermission(AevatarPermissions.Relationship.AddSubAgent, L("Permission:AddSubAgent"));
         relationshipGroup.AddPermission(AevatarPermissions.Relationship.RemoveSubAgent, L("Permission:RemoveSubAgent"));
-        relationshipGroup.AddPermission(AevatarPermissions.Relationship.RemoveAllSubAgents, L("Permission:RemoveAllSubAgents"));
+        relationshipGroup.AddPermission(AevatarPermissions.Relationship.RemoveAllSubAgents,
+            L("Permission:RemoveAllSubAgents"));
 
         // Event Management
-        var eventManagementGroup = context.AddGroup(AevatarPermissions.EventManagement.GroupName, L("Permission:EventManagement"));
+        var eventManagementGroup =
+            context.AddGroup(AevatarPermissions.EventManagement.GroupName, L("Permission:EventManagement"));
         eventManagementGroup.AddPermission(AevatarPermissions.EventManagement.Publish, L("Permission:PublishEvent"));
         eventManagementGroup.AddPermission(AevatarPermissions.EventManagement.View, L("Permission:ViewEvents"));
 
-        
+
         // Host Management
-        var hostManagementGroup = context.AddGroup(AevatarPermissions.HostManagement.GroupName, L("Permission:HostManagement")); 
-        hostManagementGroup.AddPermission(AevatarPermissions.HostManagement.Logs, L("Permission:ViewHostLogs")); 
-        
+        var hostManagementGroup =
+            context.AddGroup(AevatarPermissions.HostManagement.GroupName, L("Permission:HostManagement"));
+        hostManagementGroup.AddPermission(AevatarPermissions.HostManagement.Logs, L("Permission:ViewHostLogs"));
+
         // Cqrs Management
-        var cqrsManagementGroup = context.AddGroup(AevatarPermissions.CqrsManagement.GroupName, L("Permission:CqrsManagement"));
+        var cqrsManagementGroup =
+            context.AddGroup(AevatarPermissions.CqrsManagement.GroupName, L("Permission:CqrsManagement"));
 
         cqrsManagementGroup.AddPermission(AevatarPermissions.CqrsManagement.Logs, L("Permission:ViewLogs"));
         cqrsManagementGroup.AddPermission(AevatarPermissions.CqrsManagement.States, L("Permission:ViewStates"));
-        
-        // Subscription Management
-        var subscriptionManagementGroup = context.AddGroup(AevatarPermissions.SubscriptionManagent.GroupName, L("Permission:SubscriptionManagement"));
 
-        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.CreateSubscription, L("Permission:CreateSubscription"));
-        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.CancelSubscription, L("Permission:CancelSubscription"));
-        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.ViewSubscriptionStatus, L("Permission:ViewSubscription"));
+        // Subscription Management
+        var subscriptionManagementGroup = context.AddGroup(AevatarPermissions.SubscriptionManagent.GroupName,
+            L("Permission:SubscriptionManagement"));
+
+        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.CreateSubscription,
+            L("Permission:CreateSubscription"));
+        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.CancelSubscription,
+            L("Permission:CancelSubscription"));
+        subscriptionManagementGroup.AddPermission(AevatarPermissions.SubscriptionManagent.ViewSubscriptionStatus,
+            L("Permission:ViewSubscription"));
         var permissionInfos = GAgentPermissionHelper.GetAllPermissionInfos();
         var group = context.AddGroup("GAgents");
         foreach (var permissionInfo in permissionInfos)
-            
+
         {
             if (context.GetPermissionOrNull(permissionInfo.Name) == null)
             {
@@ -67,5 +78,4 @@ public class AevatarPermissionDefinitionProvider : PermissionDefinitionProvider
     {
         return LocalizableString.Create<AevatarResource>(name);
     }
-    
 }
