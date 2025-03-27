f412d1701d0060acd88982f8b0f9ce2c44ff9d35
house1313 <house@schrodingernft.ai>
2025-03-27 15:21:41 +0800

    test agent crud

--- regression_test.py
+++ regression_test.py
@@ -35,9 +35,7 @@ def access_token():
         data=auth_data,
         headers={"Content-Type": "application/x-www-form-urlencoded"}
     )
-    print(CLIENT_ID, CLIENT_SECRET)
-    assert response.status_code == 200
-    print(response.json()["access_token"])
+    assert_status_code(response)
     return response.json()["access_token"]
 
 
@@ -101,41 +99,42 @@ def test_login(access_token):
     assert len(access_token) > 100
 
 
-# def test_agent_crud(api_headers, test_agent):
-#     """test agent CRUD"""
-#     # get agent
-#     response = requests.get(
-#         f"{API_HOST}/api/agent/{test_agent}",
-#         headers=api_headers
-#     )
-#     assert_status_code(response)
-#     assert response.json()["data"]["name"] == AGENT_NAME
-#
-#     # update agent
-#     update_data = {
-#         "name": AGENT_NAME_MODIFIED,
-#         "properties": {"Name": AGENT_NAME_MODIFIED}
-#     }
-#     response = requests.put(
-#         f"{API_HOST}/api/agent/{test_agent}",
-#         json=update_data,
-#         headers=api_headers
-#     )
-#     assert_status_code(response)
-#     assert response.json()["data"]["name"] == AGENT_NAME_MODIFIED
-#
-#     time.sleep(2)
-#     # 验证列表
-#     response = requests.get(
-#         f"{API_HOST}/api/agent/agent-list",
-#         params={"pageIndex": 0, "pageSize": 100},
-#         headers=api_headers
-#     )
-#     assert_status_code(response)
-#     agent_ids = [agent["id"] for agent in response.json()["data"]]
-#     assert test_agent in agent_ids
-#
-#
+def test_agent_crud(api_headers, test_agent):
+    """test agent CRUD"""
+    # get agent
+    response = requests.get(
+        f"{API_HOST}/api/agent/{test_agent}",
+        headers=api_headers
+    )
+    assert_status_code(response)
+    assert response.json()["data"]["name"] == AGENT_NAME
+
+    # update agent
+    update_data = {
+        "name": AGENT_NAME_MODIFIED,
+        "properties": {"Name": AGENT_NAME_MODIFIED}
+    }
+    response = requests.put(
+        f"{API_HOST}/api/agent/{test_agent}",
+        json=update_data,
+        headers=api_headers
+    )
+    assert_status_code(response)
+    assert response.json()["data"]["name"] == AGENT_NAME_MODIFIED
+
+    # test my agent list
+    time.sleep(2)
+    response = requests.get(
+        f"{API_HOST}/api/agent/agent-list",
+        params={"pageIndex": 0, "pageSize": 100},
+        headers=api_headers
+    )
+    assert_status_code(response)
+    agent_ids = [agent["id"] for agent in response.json()["data"]]
+    assert test_agent in agent_ids
+    print("test agent crud success")
+
+
 # def test_agent_relationships(api_headers, test_agent):
 #     """test agent relationships"""
 #     # create sub agent
