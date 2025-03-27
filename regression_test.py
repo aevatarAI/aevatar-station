8e5e3ea1e9c22faa517cb384f7e03dcd40c471b2
house1313 <house@schrodingernft.ai>
2025-03-27 17:56:27 +0800

    adjust log

--- regression_test.py
+++ regression_test.py
@@ -277,16 +277,17 @@ def test_query_operations(api_headers, test_agent):
     assert response.json()["data"]["state"]["name"] == AGENT_NAME
 
     # query es
+    index_name = INDEX_NAME.lower()
     response = requests.get(
         f"{API_HOST}/api/query/es",
         params={
-            "index": INDEX_NAME,
+            "index": index_name,
             "queryString": f"name: {AGENT_NAME}",
             "pageSize": 1
         },
         headers=api_headers
     )
-    print("index name", INDEX_NAME)
+    print("index name", index_name)
     print("agent name", AGENT_NAME)
     assert_status_code(response)
     assert response.json()["data"]["totalCount"] > 0
