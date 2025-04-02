3dfcc492ccbfa7c14bd076e3c3b9bbda1dfb0b9f
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-04-02 22:19:01 +0800

    Revert "feat: tune ClientMessage."
    This reverts commit cd919e518a1d6260d19d61851284f171948bdf53.

--- src/Aevatar.SignalR/ClientMessage.cs
+++ src/Aevatar.SignalR/ClientMessage.cs
@@ -1,9 +1,4 @@
 namespace Aevatar.SignalR;
 
 [Immutable, GenerateSerializer]
-public class ClientMessage(string hubName, string connectionId, ClientNotification message)
-{
-    [Id(0)] public string HubName { get; set; } = hubName;
-    [Id(1)] public string ConnectionId { get; set; } = connectionId;
-    [Immutable] [Id(2)] public ClientNotification Message { get; set; } = message;
-}
+public sealed record ClientMessage(string HubName, string ConnectionId, [Immutable] ClientNotification Message);
