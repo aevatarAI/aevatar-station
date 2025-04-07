485942df1acac99c29c3beb9bf96017953cb76d8
Jeff-Xu23 <jeffery.xu@aelf.io>
2025-04-07 10:58:26 +0800

    feat:add api

--- src/Aevatar.HttpApi/Controllers/TokenUsageController.cs
+++ src/Aevatar.HttpApi/Controllers/TokenUsageController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Aevatar.GAgents.AI.Options;
 using Aevatar.Organizations;
 using Aevatar.Permissions;
 using Aevatar.TokenUsage;
@@ -8,6 +10,7 @@ using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Volo.Abp;
 
 namespace Aevatar.Controllers;
@@ -21,12 +24,14 @@ public class TokenUsageController: AevatarController
     private readonly ILogger<TokenUsageController> _logger;
     private readonly ITokenUsageService _tokenUsageService;
     private readonly IOrganizationPermissionChecker _organizationPermission;
+    private readonly IOptions<SystemLLMConfigOptions> _options;
 
-    public TokenUsageController(ITokenUsageService tokenUsageService, ILogger<TokenUsageController> logger, IOrganizationPermissionChecker organizationPermission)
+    public TokenUsageController(ITokenUsageService tokenUsageService, ILogger<TokenUsageController> logger, IOrganizationPermissionChecker organizationPermission, IOptions<SystemLLMConfigOptions> options)
     {
         _tokenUsageService = tokenUsageService;
         _logger = logger;
         _organizationPermission = organizationPermission;
+        _options = options;
     }
 
     [HttpPost]
@@ -35,5 +40,17 @@ public class TokenUsageController: AevatarController
         // todo: modify to organization checker
         
         return await _tokenUsageService.GetTokenUsageAsync(requestDto);
+    }
+
+    
+    [HttpGet("system-llm")]
+    public async Task<List<string>> GetSystemLLM()
+    {
+        if (_options.Value.SystemLLMConfigs == null)
+        {
+            return new List<string>();
+        }
+        
+        return _options.Value.SystemLLMConfigs.Select(s => s.Key).ToList();
     }
 }
