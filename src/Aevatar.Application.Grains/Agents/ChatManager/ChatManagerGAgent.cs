4bf765d0458e860844286486774d592db08e1918
neo887 <yunfeng.song@aelf.io>
2025-04-14 20:09:28 +0800

    feat: modify markdown prompt

--- src/Aevatar.Application.Grains/Agents/ChatManager/ChatManagerGAgent.cs
+++ src/Aevatar.Application.Grains/Agents/ChatManager/ChatManagerGAgent.cs
@@ -297,17 +297,14 @@ public class ChatGAgentManager : AIGAgentBase<ChatManagerGAgentState, ChatManage
         //sysMessage = await AppendUserInfoToSystemPromptAsync(configuration, sysMessage, userProfile);
         var formattedRequirement =
             """
-            ### 按 Markdown 
             ### 如果有数学公式，按如下格式处理：
-            1. 行内LaTeX公式使用@@@符号和===符号包裹,
-                示例：@@@ LaTeX公式 ===
-                示例：@@@E=mc^2===
+            1. 行内LaTeX公式使用@@@$和$@@@符号包裹,
+                示例：@@@$LaTeX公式$@@@
+                示例：@@@$E=mc^2$@@@
                 注意：不是@@@E=mc^2@@@
-            2. 块级LaTeX公式用 aelfstart\LaTeX公式aelfend 包裹,
-                示例：aelfstart\LaTeX公式aelfend
-                示例：aelfstart\int_a^b f(x)dxaelfend
-                示例：aelfstartM = R \cdot (I + A)aelfend
-                注意：不用 '$$'、'['、'\[' 包裹    
+            2. 块级LaTeX公式用 ===$$\LaTeX公式$$=== 包裹,
+                示例：===$$\int_a^b f(x)dx$$===
+                示例：===$$M = R \cdot (I + A)$$=== 
             """;
         
        
