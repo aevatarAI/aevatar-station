d46b0a3abe03791d74ca5b692d5357b1c272e914
neo887 <yunfeng.song@aelf.io>
2025-04-16 22:56:18 +0800

    feat: add log

--- src/Aevatar.Application.Grains/Agents/ChatManager/ChatManagerGAgent.cs
+++ src/Aevatar.Application.Grains/Agents/ChatManager/ChatManagerGAgent.cs
@@ -479,9 +479,9 @@ public class ChatGAgentManager : AIGAgentBase<ChatManagerGAgentState, ChatManage
 
     public async Task<List<ChatMessage>> GetSessionMessageListAsync(Guid sessionId)
     {
-        Logger.LogDebug($"[ChatGAgentManager][GetSessionMessageListAsync] - session:ID{sessionId.ToString()}");
+        Logger.LogDebug($"[ChatGAgentManager][GetSessionMessageListAsync] - session:ID {sessionId.ToString()}");
         var sessionInfo = State.GetSession(sessionId);
-        Logger.LogDebug($"[ChatGAgentManager][GetSessionMessageListAsync] - session:ID{JsonConvert.SerializeObject(sessionInfo)}");
+        Logger.LogDebug($"[ChatGAgentManager][GetSessionMessageListAsync] - session:ID {JsonConvert.SerializeObject(sessionInfo)}");
 
         if (sessionInfo == null)
         {
