f5aebc7ea1df4118bdf17dddcc0718bd129c16de
neo887 <yunfeng.song@aelf.io>
2025-04-23 15:17:48 +0800

    feat: region to llm

--- src/Aevatar.Application.Grains/Agents/ChatManager/ChatAgent/GodChatGAgent.cs
+++ src/Aevatar.Application.Grains/Agents/ChatManager/ChatAgent/GodChatGAgent.cs
@@ -27,7 +27,7 @@ public class GodChatGAgent : ChatGAgentBase<GodChatState, GodChatEventLog, Event
     private static readonly Dictionary<string, List<string>> RegionToLLMsMap = new Dictionary<string, List<string>>()
     {
         { "CN", new List<string> { "DeepSeek"} },
-        { "DEFAULT", new List<string>() {  "OpenAI", "OpenAILast" }}
+        { "DEFAULT", new List<string>() {  "OpenAILast", "OpenAI" }}
     };
     private static readonly TimeSpan RequestRecoveryDelay = TimeSpan.FromSeconds(600);
     private const string DefaultRegion = "DEFAULT";
