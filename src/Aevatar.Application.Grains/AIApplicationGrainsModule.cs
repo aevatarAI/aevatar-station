413af19f5a8fd6d15ba841ecce74342714f7f229
jecket2 <142502649+jecket2@users.noreply.github.com>
2025-04-24 16:26:54 +0800

    Update AIApplicationGrainsModule.cs

--- src/Aevatar.Application.Grains/AIApplicationGrainsModule.cs
+++ src/Aevatar.Application.Grains/AIApplicationGrainsModule.cs
@@ -14,7 +14,8 @@ namespace Aevatar.Application.Grains;
     typeof(AevatarApplicationContractsModule),
     typeof(AevatarCQRSModule),
     typeof(AevatarNeo4JStoreModule),
-    typeof(AevatarGAgentsTwitterModule)
+    typeof(AevatarGAgentsTwitterModule),
+    typeof(AevatarGAgentsTelegramModule)
 )]
 public class AIApplicationGrainsModule : AbpModule
  
