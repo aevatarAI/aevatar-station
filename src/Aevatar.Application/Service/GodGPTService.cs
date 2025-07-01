aef3e0b61783e90f810151d25f78ad041a05f7be
yann <feng.yan@aelf.io>
2025-07-01 11:54:34 +0800

    using System.Linq;

--- src/Aevatar.Application/Service/GodGPTService.cs
+++ src/Aevatar.Application/Service/GodGPTService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Aevatar.Application.Grains.Agents.ChatManager;
 using Aevatar.Application.Grains.Agents.ChatManager.Chat;
