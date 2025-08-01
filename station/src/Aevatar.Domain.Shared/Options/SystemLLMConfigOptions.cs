15896b0f3f9b1db5fcd2c62c0e571cd3b1f26def
chent00ng <chen.tang@aelf.io>
2025-08-01 17:01:40 +0800

    update SystemLLMConfigOptions

--- station/src/Aevatar.Domain.Shared/Options/SystemLLMConfigOptions.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Aevatar.Options;
-
-public class SystemLLMConfigOptions
-{
-    public List<string> SystemLLMConfigs { get; set; } = new() { "OpenAI", "DeepSeek", "AzureOpenAI", "AzureOpenAIEmbeddings", "OpenAIEmbeddings" };
-} 
