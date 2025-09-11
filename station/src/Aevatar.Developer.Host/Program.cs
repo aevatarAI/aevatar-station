611f5ce302a9029f1b43b6503d8e9c2fc527cab6
chao0kang <chao.kang@aelf.io>
2025-09-11 16:31:40 +0800

    feat: change linkTask to httpApi

--- station/src/Aevatar.Developer.Host/Program.cs
+++ station/src/Aevatar.Developer.Host/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Aevatar.Background;
 using Aevatar.Developer.Host.Extensions;
 using Aevatar.SignalR;
 using Microsoft.AspNetCore.Builder;
@@ -33,7 +32,6 @@ public class Program
                 .UseAutofac()
                 .UseSerilog();
             builder.Services.AddSignalR().AddOrleans();
-            builder.Services.AddHostedService<DocumentLinkScheduledTask>();
             await builder.AddApplicationAsync<AevatarDeveloperHostModule>();
             var app = builder.Build();
             await app.InitializeApplicationAsync();
