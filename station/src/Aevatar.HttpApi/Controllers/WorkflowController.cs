90b4371e137c3244176ef4c053e35a42067290ee
chent00ng <chen.tang@aelf.io>
2025-09-15 17:38:41 +0800

    fix build problem

--- station/src/Aevatar.HttpApi/Controllers/WorkflowController.cs
+++ station/src/Aevatar.HttpApi/Controllers/WorkflowController.cs
@@ -12,7 +12,7 @@ namespace Aevatar.Controllers
     /// </summary>
     [ApiController]
     [Route("api/workflow")]
-    // [Authorize]
+    [Authorize]
     public class WorkflowController : AbpControllerBase
     {
         private readonly IWorkflowOrchestrationService _workflowOrchestrationService;
