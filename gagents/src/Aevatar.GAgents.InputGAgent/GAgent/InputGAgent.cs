e06c10b5c527c5a0d0140313576af0c628a7bdc5
louis.li <louis.li@aelf.io>
2025-09-16 10:22:12 +0800

    feat:solve conflicts

--- gagents/src/Aevatar.GAgents.InputGAgent/GAgent/InputGAgent.cs
+++ gagents/src/Aevatar.GAgents.InputGAgent/GAgent/InputGAgent.cs
@@ -26,9 +26,6 @@ public class InputGAgent : MemberGAgentBase<InputGAgentState, InputGAgentLogEven
     [Interceptor(IsWorkflowStep = true)]
     protected override Task<int> GetInterestValueAsync(Guid blackboardId)
     {
-        // DEBUG: 添加调试信息来验证方法是否被调用和拦截器状态
-        Logger.LogInformation("[DEBUG InputGAgent.GetInterestValueAsync] Method called - WorkflowId={WorkflowId}, BlackboardId={BlackboardId}", 
-            WorkflowId, blackboardId);
         
         return Task.FromResult(100);
     }
@@ -36,10 +33,6 @@ public class InputGAgent : MemberGAgentBase<InputGAgentState, InputGAgentLogEven
     [Interceptor(IsWorkflowStep = true)]
     protected override Task<ChatResponse> ChatAsync(Guid blackboardId, List<ChatMessage>? messages)
     {
-        // DEBUG: 添加调试信息来验证方法是否被调用和拦截器状态
-        Logger.LogInformation("[DEBUG InputGAgent.ChatAsync] Method called - WorkflowId={WorkflowId}, BlackboardId={BlackboardId}, MessagesCount={MessagesCount}", 
-            WorkflowId, blackboardId, messages?.Count ?? 0);
-        
         var response = new ChatResponse
         {
             Content = State.Input,
@@ -58,10 +51,6 @@ public class InputGAgent : MemberGAgentBase<InputGAgentState, InputGAgentLogEven
     [Interceptor(IsWorkflowStep = true)]
     protected override async Task PerformConfigAsync(InputConfigDto configuration)
     {
-        // DEBUG: 添加调试信息来验证方法是否被调用和拦截器状态
-        Logger.LogInformation("[DEBUG InputGAgent.PerformConfigAsync] Method called - WorkflowId={WorkflowId}, MemberName={MemberName}, Input={Input}", 
-            WorkflowId, configuration?.MemberName, configuration?.Input);
-        
         await base.PerformConfigAsync(configuration);
         
         RaiseEvent(new SetInputLogEvent { Input = configuration.Input });
