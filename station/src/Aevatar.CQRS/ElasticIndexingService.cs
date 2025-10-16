7e9f162e0b248c103abb560e5e0241202cb4e9d9
feiniao55 <feiniao_55@163.com>
2025-10-16 10:43:02 +0800

    feat: skip dictionary parsing when saving state

--- station/src/Aevatar.CQRS/ElasticIndexingService.cs
+++ station/src/Aevatar.CQRS/ElasticIndexingService.cs
@@ -183,21 +183,23 @@ public class ElasticIndexingService : IIndexingService, ISingletonDependency
     {
         Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
 
-        if (underlyingType.IsGenericType)
-        {
-            var genericDef = underlyingType.GetGenericTypeDefinition();
-            if (SupportedCollectionTypes.Contains(genericDef))
-            {
-                var genericArgs = underlyingType.GetGenericArguments();
-                if (genericArgs.Length == 2 && SupportedDictionaryTypes.Contains(genericDef))
-                {
-                    return IsBasicType(genericArgs[0]) && IsBasicType(genericArgs[1]);
-                }
-                
-                return IsBasicType(genericArgs[0]);
-            }
-        }
-
+        //Skip dictionary parsing when saving state.  start
+        // if (underlyingType.IsGenericType)
+        // {
+        //     var genericDef = underlyingType.GetGenericTypeDefinition();
+        //     if (SupportedCollectionTypes.Contains(genericDef))
+        //     {
+        //         var genericArgs = underlyingType.GetGenericArguments();
+        //         if (genericArgs.Length == 2 && SupportedDictionaryTypes.Contains(genericDef))
+        //         {
+        //             return IsBasicType(genericArgs[0]) && IsBasicType(genericArgs[1]);
+        //         }
+        //         
+        //         return IsBasicType(genericArgs[0]);
+        //     }
+        // }
+        //end 
+        
         if (underlyingType.IsPrimitive)
             return true;
 
